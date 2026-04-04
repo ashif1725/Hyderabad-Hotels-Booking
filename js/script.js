e783ab5381072faefbca705df63ee83ab19d8af4
ashif1725 <ashif9720@gmail.com>
2026-04-04 18:46:24 +0530

    Remove unused functions from script.js
    Removed searchHotel and sendWhatsApp functions along with their associated code.
    
    Signed-off-by: ashif1725 <ashif9720@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 const images = [
   "https://images.unsplash.com/photo-1501117716987-c8e1ecb210b5",
   "https://images.unsplash.com/photo-1566073771259-6a8506099945",
@@ -11,27 +10,3 @@ setInterval(() => {
   i = (i + 1) % images.length;
   document.getElementById("slide").src = images[i];
 }, 3000);
-
-function searchHotel() {
-  alert("Search feature coming soon 😎");
-}
-<script>
-function sendWhatsApp() {
-  var name = document.getElementById("name").value;
-  var phone = document.getElementById("phone").value;
-  var hotel = document.getElementById("hotel").value;
-  var checkin = document.getElementById("checkin").value;
-  var checkout = document.getElementById("checkout").value;
-
-  var message = "New Booking:%0A"
-              + "Name: " + name + "%0A"
-              + "Phone: " + phone + "%0A"
-              + "Hotel: " + hotel + "%0A"
-              + "Check-in: " + checkin + "%0A"
-              + "Check-out: " + checkout;
-
-  var whatsappUrl = "https://wa.me/91YOURNUMBER?text=" + message;
-
-  window.open(whatsappUrl, "_blank");
-}
-</script>
